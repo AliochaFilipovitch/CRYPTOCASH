a8b57482be1557fea19079109d7b80fd0c259ae5
AliochaFilipovitch <alexis.queune1@gmail.com>
2018-03-02 11:34:38 +0100

    Add card best

--- js/refresh.js
+++ js/refresh.js
@@ -1,5 +1,6 @@
 var a = 0;
 var b = 0;
+var c = 0;
 
 $(document).ready(function() {
   $('#refreshTime').click(function(){
@@ -10,4 +11,8 @@ $(document).ready(function() {
         b = 1;
         $.getScript("http://localhost/CRYPTOCASH/js/average.js");
     });
+   $('#refreshBest').click(function(){
+        c = 1;
+        $.getScript("http://localhost/CRYPTOCASH/js/best.js");
+   });
 });
